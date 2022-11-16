fa9b3492becbab57ee08a654b81ecd2df2128c35
Daniela Armijo <111472176+Dany1970@users.noreply.github.com>
2022-11-16 13:40:20 -0300

    Add files via upload

---
+++ JavaApplication38.java
@@ -0,0 +1,19 @@
+/*
+probando con un repositorio totalmente nuevo fuera de tecnicaturagit
+ */
+package javaapplication38;
+
+/**
+ *
+ * @author dania
+ */
+public class JavaApplication38 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Man at work");
+    }
+    
+}
